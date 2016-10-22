82fc0daa08e0c1fcd31d4a59ab022137d9f181cd
Stefano Kals <stefano.kals@bluewin.ch>
2016-10-22 17:25:16 +0200

    listener in app.js added to test environment

--- app.js
+++ app.js
@@ -58,3 +58,7 @@ app.use(function (err, req, res, next) {
 
 
 module.exports = app;
+
+const hostname = '127.0.0.1';
+const port = 3001;
+app.listen(port, hostname, () => {  console.log(`Server running at http://${hostname}:${port}/`); });
