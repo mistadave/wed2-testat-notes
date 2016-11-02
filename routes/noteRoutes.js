8dd8761096caaed1396fcbb4763d0c97ab4c9b02
David Stäheli <mistrdave@gmail.com>
2016-11-02 17:23:06 +0100

    middleware added to index :smile:

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var notes = require('../controller/notesController.js');
-
-router.get("/", notes.showIndex);
+var style = require('../services/styleSwitcher');
+router.get("/", style.changeStyle, notes.showIndex);
 router.get("/changeStyle", notes.changeStyle);
-router.get("/newNote", notes.showNewNote);
+router.get("/notes/create", notes.showNewNote);
 router.get("/notes", notes.getNotes); //get all the notes
-router.get("/notes/:id/", notes.getNote); //get one note by id
+router.get("/notes/:id", notes.getNote); //get one note by id
 // TODO: POST: sent as JSON object in body works! Tested with POSTMAN.
 router.post("/notes", notes.createNote); //insert new note
-router.delete("/notes/:id/", notes.deleteNote); //delete a note by id
+router.delete("/notes/:id", notes.deleteNote); //delete a note by id
 
 
 /*
