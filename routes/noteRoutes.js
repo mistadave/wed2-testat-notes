a0d042ad44eee0c84ca629a029a53fd14216d547
David Stäheli <mistrdave@gmail.com>
2016-11-16 12:33:26 +0100

    removed usless comments :grin:

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -6,11 +6,10 @@ var sort = require('../services/sortNotes.js');
 router.get("/", sort.sortNotes, notes.getNotes);
 router.get("/changeStyle", style.changeStyle);
 router.get("/notes/new", notes.showNewNote);
-router.get("/notes", notes.getNotes); //get all the notes
-router.get("/notes/:id", notes.getNote); //get one note by id
-router.post("/notes/create", notes.createNote); //insert new note
-router.post("/notes/edit/:id", notes.updateNote); //update note by id
-//  router.get("/notes/delete/:id", notes.deleteNote); //delete a note by id
+router.get("/notes", notes.getNotes);
+router.get("/notes/:id", notes.getNote);
+router.post("/notes/create", notes.createNote);
+router.post("/notes/edit/:id", notes.updateNote);
 router.get('/sortNotes/:name', sort.sortNotes, notes.sortedNotes);
 
 module.exports = router;
