99362c68274d43f68ba7d7e43d7715b92a3e3083
David Stäheli <mistrdave@gmail.com>
2016-11-16 10:53:56 +0100

    redirect after create note :smile:

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -8,11 +8,9 @@ router.get("/changeStyle", style.changeStyle);
 router.get("/notes/new", notes.showNewNote);
 router.get("/notes", notes.getNotes); //get all the notes
 router.get("/notes/:id", notes.getNote); //get one note by id
-router.post("/notes/create", notes.createNote, notes.getNotes); //insert new note
+router.post("/notes/create", notes.createNote); //insert new note
 router.post("/notes/edit/:id", notes.updateNote); //update note by id
-router.get("/notes/delete/:id", notes.deleteNote); //delete a note by id
+//  router.get("/notes/delete/:id", notes.deleteNote); //delete a note by id
 router.get('/sortNotes/:name', sort.sortNotes, notes.sortedNotes);
-
-//TODO: delte not working! Workaraound notes/delete/...
 
 module.exports = router;
